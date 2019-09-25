a45432092db89ff4479867cf08ce9116dc969ad2
steadfast-motherboard-7317 <imadejr@yahoo.com>
2019-09-25 22:56:39 +0000

    Automatically backed up by Learn

--- spec/array_methods_spec.rb
+++ spec/array_methods_spec.rb
@@ -13,8 +13,8 @@ end
 describe "#using_sort" do 
   it "takes in an argument of an array and returns the sorted array." do 
     array = ["wow", "I", "am", "really", "learning", "arrays!"]
-    expect(using_sort(array).last).to eq("am")
-    expect(using_sort(array).first).to eq("I")
+    expect(using_sort(array).last).to eq("wow")
+    expect(using_sort(array).first).to eq("am")
   end
 end
 
